838cbf956aa8b9e341ea57639e275660aa408cf7
LyotoRui <80151467+LyotoRui@users.noreply.github.com>
2022-01-15 23:58:03 +0200

    main

--- 4.py
+++ 4.py
@@ -1,7 +1,3 @@
-from operator import length_hint
-from turtle import width
-
-
 width = input('Enter homestead`s width, ft: ')
 length = input('Enter homestead`s length, ft: ')
 print(f'{(float(width) * float(length)) / 43560} acre')
